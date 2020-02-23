f3e7e4e284380513f863f9a6a8d7cb1a52d608c3
Morris <morris13579@gmail.com>
2020-02-23 11:23:50 +0800

    add check ischool plus cookies

--- lib/src/taskcontrol/task/CheckCookiesTask.dart
+++ lib/src/taskcontrol/task/CheckCookiesTask.dart
@@ -1,8 +1,11 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/debug/log/Log.dart';
 import 'package:flutter_app/generated/i18n.dart';
 import 'package:flutter_app/src/connector/CourseConnector.dart';
 import 'package:flutter_app/src/connector/ISchoolConnector.dart';
+import 'package:flutter_app/src/connector/ISchoolPlusConnector.dart';
 import 'package:flutter_app/src/connector/NTUTConnector.dart';
 import 'package:flutter_app/src/taskcontrol/task/TaskModel.dart';
 import 'package:flutter_app/ui/other/MyProgressDialog.dart';
@@ -13,6 +16,7 @@ class CheckCookiesTask extends TaskModel {
   String studentId;
   static String checkCourse = "__Course__";
   static String checkISchool = "__ISchool__";
+  static String checkPlusISchool = "__ISchoolPlus__";
   static String checkNTUT = "__NTUT__";
   static String checkScore = "__Score__";
 
@@ -26,11 +30,17 @@ class CheckCookiesTask extends TaskModel {
     Log.d(checkSystem);
     MyProgressDialog.showProgressDialog(context, S.current.checkLogin);
     bool isLoginCourse = true;
-    bool isLoginSchool = true;
+    bool isLoginISchool = true;
     bool isLoginNTUT = true;
+    bool isLoginISchoolPlus = true;
     bool checkCourseSystem = checkSystem.contains(checkCourse);
     bool checkISchoolSystem = checkSystem.contains(checkISchool);
     bool checkNTUTSystem = checkSystem.contains(checkNTUT);
+    bool checkISchoolPlusSystem = checkSystem.contains(checkPlusISchool);
+    if(checkISchoolPlusSystem ){
+      ISchoolPlusConnector.loginFalse();
+      isLoginISchoolPlus = await ISchoolPlusConnector.checkLogin();
+    }
     if (checkCourseSystem || checkISchoolSystem || checkNTUTSystem) {
       NTUTConnector.loginFalse();
       isLoginNTUT = await NTUTConnector.checkLogin();
@@ -49,10 +59,10 @@ class CheckCookiesTask extends TaskModel {
       isLoginCourse = await CourseConnector.checkLogin();
     }
     if (checkISchoolSystem) {
-      isLoginSchool = await ISchoolConnector.checkLogin(studentId: studentId);
+      isLoginISchool = await ISchoolConnector.checkLogin(studentId: studentId);
     }
     MyProgressDialog.hideProgressDialog();
-    if (isLoginSchool && isLoginCourse) {
+    if (isLoginISchool && isLoginCourse & isLoginISchoolPlus ) {
       return TaskStatus.TaskSuccess;
     } else {
       return TaskStatus.TaskFail;
