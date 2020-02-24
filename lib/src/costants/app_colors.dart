6d6b89f1b37c0e00c9a51c53cd88e0ec4bbf9881
yaoandy107 <yaoandy107@gmail.com>
2020-02-24 17:49:12 +0800

    Add global constants of colors

---
+++ lib/src/costants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // color
+  static const Color mainColor = Colors.blue;
+
+  // font color
+  static const Color darkFontColor = Colors.black87;
+  static const Color lightFontColor = Colors.white;
+  static final Color greyFontColor = Colors.grey[700];
+}
