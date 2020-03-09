62a7d58add79cd13ce7a213031d7b2811734b8c1
yaoandy107 <yaoandy107@gmail.com>
2020-03-09 18:43:36 +0800

    Update l10n

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -1448,7 +1448,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
 
   List<Locale> get supportedLocales {
     return const <Locale>[
-      Locale('en', ''), Locale('zh', ''),
+      Locale('zh', ''), Locale('en', ''),
     ];
   }
 
