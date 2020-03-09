f1477bbaff33618e29d2aa0c1aac4f500d30575b
morris13579 <morris13579@gmail.com>
2020-03-09 16:37:34 +0800

    modify some file dir

--- lib/src/taskcontrol/task/ischool/ISchoolCourseFileTask.dart
+++ lib/src/taskcontrol/task/ischool/ISchoolCourseFileTask.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/src/R.dart';
 import 'package:flutter_app/src/connector/ISchoolConnector.dart';
 import 'package:flutter_app/src/store/Model.dart';
-import 'package:flutter_app/src/store/json/CourseFileJson.dart';
+import 'package:flutter_app/src/store/object/CourseFileJson.dart';
 import 'package:flutter_app/src/taskcontrol/task/CheckCookiesTask.dart';
 import 'package:flutter_app/src/taskcontrol/task/TaskModel.dart';
 import 'package:flutter_app/ui/other/ErrorDialog.dart';
