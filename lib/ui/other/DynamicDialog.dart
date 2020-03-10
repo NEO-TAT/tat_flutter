c10035f4b5755ea850f790447d4a0e38914c33cc
Morris <morris13579@gmail.com>
2020-03-10 15:44:51 +0800

    add getYearList

--- lib/ui/other/DynamicDialog.dart
+++ lib/ui/other/DynamicDialog.dart
@@ -1,77 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-String _dialogMessage = "Loading...";
-double _progress = 0.0, _maxProgress = 100.0;
-
 bool _isShowing = false;
 BuildContext _context, _dismissingContext;
-bool _barrierDismissible = true, _showLogs = false;
-
-TextStyle _progressTextStyle = TextStyle(
-    color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.w400),
-    _messageStyle = TextStyle(
-        color: Colors.black, fontSize: 19.0, fontWeight: FontWeight.w600);
-
-double _dialogElevation = 8.0, _borderRadius = 8.0;
-Color _backgroundColor = Colors.white;
-Curve _insetAnimCurve = Curves.easeInOut;
-
-Widget _progressWidget = Image.asset(
-  'assets/double_ring_loading_io.gif',
-  package: 'progress_dialog',
-);
+bool _barrierDismissible = true;
+String _dialogMessage = "Loading...";
+String _progressString =  "0/100";
+double _progress = 0;
 
 class DynamicDialog {
   _Body _dialog;
 
-  DynamicDialog(BuildContext context,
-      {bool isDismissible, bool showLogs}) {
+  DynamicDialog(BuildContext context, {bool isDismissible}) {
     _context = context;
     _barrierDismissible = isDismissible ?? true;
-    _showLogs = showLogs ?? false;
   }
 
-  void style(
-      {double progress,
-        double maxProgress,
-        String message,
-        Widget progressWidget,
-        Color backgroundColor,
-        TextStyle progressTextStyle,
-        TextStyle messageTextStyle,
-        double elevation,
-        double borderRadius,
-        Curve insetAnimCurve}) {
-    if (_isShowing) return;
-
+  void update({String message, double nowProgress , String progressString}) {
+    _progress = nowProgress ?? _progress;
     _dialogMessage = message ?? _dialogMessage;
-    _maxProgress = maxProgress ?? _maxProgress;
-    _progressWidget = progressWidget ?? _progressWidget;
-    _backgroundColor = backgroundColor ?? _backgroundColor;
-    _messageStyle = messageTextStyle ?? _messageStyle;
-    _progressTextStyle = progressTextStyle ?? _progressTextStyle;
-    _dialogElevation = elevation ?? _dialogElevation;
-    _borderRadius = borderRadius ?? _borderRadius;
-    _insetAnimCurve = insetAnimCurve ?? _insetAnimCurve;
-  }
-
-  void update(
-      {double nowProgress,
-        double progress,
-        double maxProgress,
-        String message,
-        Widget progressWidget,
-        TextStyle progressTextStyle,
-        TextStyle messageTextStyle}) {
-    _progress  = nowProgress ?? _progress;
-    _dialogMessage = message ?? _dialogMessage;
-    _maxProgress = maxProgress ?? _maxProgress;
-    _progressWidget = progressWidget ?? _progressWidget;
-    _messageStyle = messageTextStyle ?? _messageStyle;
-    _progressTextStyle = progressTextStyle ?? _progressTextStyle;
-
+    _progressString = progressString ?? _progressString;
     if (_isShowing) _dialog.update();
   }
 
@@ -85,13 +33,8 @@ class DynamicDialog {
         _isShowing = false;
         if (Navigator.of(_dismissingContext).canPop()) {
           Navigator.of(_dismissingContext).pop();
-          if (_showLogs) debugPrint('ProgressDialog dismissed');
-        } else {
-          if (_showLogs) debugPrint('Cant pop ProgressDialog');
         }
       } catch (_) {}
-    } else {
-      if (_showLogs) debugPrint('ProgressDialog already dismissed');
     }
   }
 
@@ -100,13 +43,11 @@ class DynamicDialog {
       try {
         _isShowing = false;
         Navigator.of(_dismissingContext).pop(true);
-        if (_showLogs) debugPrint('ProgressDialog dismissed');
         return Future.value(true);
       } catch (_) {
         return Future.value(false);
       }
     } else {
-      if (_showLogs) debugPrint('ProgressDialog already dismissed');
       return Future.value(false);
     }
   }
@@ -123,13 +64,9 @@ class DynamicDialog {
             return WillPopScope(
               onWillPop: () async => _barrierDismissible,
               child: Dialog(
-                  backgroundColor: _backgroundColor,
-                  insetAnimationCurve: _insetAnimCurve,
                   insetAnimationDuration: Duration(milliseconds: 100),
-                  elevation: _dialogElevation,
                   shape: RoundedRectangleBorder(
-                      borderRadius:
-                      BorderRadius.all(Radius.circular(_borderRadius))),
+                      borderRadius: BorderRadius.all(Radius.circular(8))),
                   child: _dialog),
             );
           },
@@ -137,14 +74,12 @@ class DynamicDialog {
         // Delaying the function for 200 milliseconds
         // [Default transitionDuration of DialogRoute]
         await Future.delayed(Duration(milliseconds: 200));
-        if (_showLogs) debugPrint('ProgressDialog shown');
         _isShowing = true;
         return true;
       } catch (_) {
         return false;
       }
     } else {
-      if (_showLogs) debugPrint("ProgressDialog already shown/showing");
       return false;
     }
   }
@@ -172,7 +107,6 @@ class _BodyState extends State<_Body> {
   @override
   void dispose() {
     _isShowing = false;
-    if (_showLogs) debugPrint('ProgressDialog dismissed by back button');
     super.dispose();
   }
 
@@ -181,16 +115,21 @@ class _BodyState extends State<_Body> {
     return Container(
       height: 50,
       child: Column(
-        mainAxisSize : MainAxisSize.min,
+        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Expanded(
-            child: Text(_dialogMessage),
-          ),
+          Text(_dialogMessage),
           LinearProgressIndicator(
             backgroundColor: Colors.grey[200],
             valueColor: AlwaysStoppedAnimation(Colors.blue),
             value: _progress,
           ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text( _progressString , textAlign: TextAlign.center),
+              ),
+            ],
+          )
         ],
       ),
     );
