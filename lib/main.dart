2a884342fb52448a1f293b91cfaeb0408afda865
yaoandy107 <yaoandy107@gmail.com>
2020-03-25 16:40:30 +0800

    Disable the crashlytics at dev mode

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,6 @@ import 'debug/log/Log.dart';
 import 'generated/l10n.dart';
 
 Future<Null> main() async {
-  // Set `enableInDevMode` to true to see reports while in debug mode
-  // This is only to be used for confirming that reports are being
-  // submitted as expected. It is not intended to be used for everyday
-  // development.
-  Crashlytics.instance.enableInDevMode = true;
 
   // Pass all uncaught errors from the framework to Crashlytics.
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
