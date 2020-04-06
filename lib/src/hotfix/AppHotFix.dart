b226028cc63605566682eed57b7f37944b4ced10
morris13579 <morris13579@gmail.com>
2020-04-06 13:29:25 +0800

    add app hotfix

--- lib/src/hotfix/AppHotFix.dart
+++ lib/src/hotfix/AppHotFix.dart
@@ -1,9 +1,25 @@
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppHotFix{
-  Directory getUpdatePath() async{
-    Directory dir = await getExternalCacheDirectories();
+  Future<String> _getUpdatePath() async{
+    Directory dir = await getExternalStorageDirectory();
+    return dir.path;
   }
+
+  Future<String> getPatchVersion() async{
+    var pref = await SharedPreferences.getInstance();
+    String version = pref.getString("patch_version");
+    version = version ?? "";
+    return version;
+  }
+
+  void checkPatchVersion(){
+
+  }
+
+
+
 }
