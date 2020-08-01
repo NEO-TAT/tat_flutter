86790dbfe6bc46dd41e7310a064e654934da5ddd
morris13579 <morris13579@gmail.com>
2020-07-31 20:46:56 -0700

    fix ios build fail

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -25,6 +25,6 @@ import Firebase
 
 private func registerPlugins(registry: FlutterPluginRegistry) {
     if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
-       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin"))
+        FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin") as! FlutterPluginRegistrar)
     }
 }
