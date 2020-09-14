025fc2ceb7cf01a3b0ae4adb173bc3a169a685a1
morris13579 <morris13579@gmail.com>
2020-09-14 21:36:26 +0800

    add teacher course table

--- lib/src/taskcontrol/task/course/CourseSemesterTask.dart
+++ lib/src/taskcontrol/task/course/CourseSemesterTask.dart
@@ -18,9 +18,19 @@ class CourseSemesterTask extends TaskModel {
 
   @override
   Future<TaskStatus> taskStart() async {
-    MyProgressDialog.showProgressDialog(context, R.current.getCourseSemester);
-    List<SemesterJson> value = await CourseConnector.getCourseSemester(id);
-    MyProgressDialog.hideProgressDialog();
+    List<SemesterJson> value;
+    if (id.length == 5) {
+      value = List();
+      DateTime dateTime = DateTime.now();
+      int year = dateTime.year - 1911;
+      int semester = (dateTime.month <= 8 && dateTime.month >= 1) ? 2 : 1;
+      value.add(
+          SemesterJson(semester: semester.toString(), year: year.toString()));
+    } else {
+      MyProgressDialog.showProgressDialog(context, R.current.getCourseSemester);
+      value = await CourseConnector.getCourseSemester(id);
+      MyProgressDialog.hideProgressDialog();
+    }
     if (value != null) {
       Model.instance.setSemesterJsonList(value);
       return TaskStatus.TaskSuccess;
