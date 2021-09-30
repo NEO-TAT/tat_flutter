cc56331619fa494f97c0c84807f3ef02957fa5f7
Xanonymous <trusaidlin@gmail.com>
2021-10-01 00:42:02 +0800

    upgrade ntut connector

--- lib/src/connector/ntut_connector.dart
+++ lib/src/connector/ntut_connector.dart
@@ -1,14 +1,10 @@
-//
-//  ntut_connector.dart
 //  北科課程助手
-//
-//  Created by morris13579 on 2020/02/12.
-//  Copyright © 2020 morris13579 All rights reserved.
-//
 
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:html/parser.dart';
+import 'package:intl/intl.dart';
 import 'package:tat/debug/log/log.dart';
 import 'package:tat/src/connector/core/connector.dart';
 import 'package:tat/src/connector/core/connector_parameter.dart';
@@ -16,64 +12,71 @@ import 'package:tat/src/model/ntut/ap_tree_json.dart';
 import 'package:tat/src/model/ntut/ntut_calendar_json.dart';
 import 'package:tat/src/model/userdata/user_data_json.dart';
 import 'package:tat/src/store/model.dart';
-import 'package:html/dom.dart';
-import 'package:html/parser.dart';
-import 'package:intl/intl.dart';
 
 enum NTUTConnectorStatus {
   LoginSuccess,
   PasswordExpiredWarning,
-  AccountLockWarning, //帳號鎖住
+  AccountLockWarning,
   AccountPasswordIncorrect,
-  AuthCodeFailError, //驗證碼錯誤
+  AuthCodeFailError,
   UnknownError
 }
 
 class OrgtreeSearchResult {
-  String id;
-  String name;
-  String msg;
+  OrgtreeSearchResult({
+    this.id,
+    this.name,
+    this.msg,
+  });
+
+  String? id;
+  String? name;
+  String? msg;
 }
 
 class NTUTConnector {
-  static final String host = "https://app.ntut.edu.tw/";
-  static final String _loginUrl = host + "login.do";
-  static final String _getPictureUrl = host + "photoView.do";
-  static final String _getTreeUrl = host + "aptreeList.do";
-  static final String _getCalendarUrl = host + "calModeApp.do";
-  static final String _changePasswordUrl = host + "passwordMdy.do";
-  static final String _orgtreeSearchUrl = host + "orgtreeSearch.do";
+  static const host = "https://app.ntut.edu.tw/";
+  static const _loginUrl = host + "login.do";
+  static const _getPictureUrl = host + "photoView.do";
+  static const _getTreeUrl = host + "aptreeList.do";
+  static const _getCalendarUrl = host + "calModeApp.do";
+  static const _changePasswordUrl = host + "passwordMdy.do";
+  static const _orgtreeSearchUrl = host + "orgtreeSearch.do";
 
   static Future<NTUTConnectorStatus> login(
-      String account, String password) async {
+    String account,
+    String password,
+  ) async {
     try {
-      ConnectorParameter parameter;
-      Map<String, String> data = {
+      final data = {
         "muid": account,
         "mpassword": password,
         "forceMobile": "app",
-        //"rememberUidValue": "" ,
-        //"rememberPwdValue" : "1" ,
-        //"authcode" : "" ,
         "md5Code": "1111",
         "ssoId": ""
       };
-      parameter = ConnectorParameter(_loginUrl);
-      parameter.data = data;
-      parameter.userAgent = "Direk Android App";
-      String result = await Connector.getDataByPost(parameter);
-      parameter.userAgent = "";
-      Map jsonMap = json.decode(result);
+
+      final parameter = ConnectorParameter(_loginUrl)
+        ..data = data
+        ..userAgent = 'Direk Android App'
+        ..userAgent = '';
+
+      final result = await Connector.getDataByPost(parameter);
+      final jsonMap = json.decode(result);
+
       if (jsonMap["success"]) {
-        UserInfoJson userInfo = UserInfoJson.fromJson(jsonMap);
+        final userInfo = UserInfoJson.fromJson(jsonMap);
         Model.instance.setUserInfo(userInfo);
         Model.instance.saveUserData();
+
         if (userInfo.passwordExpiredRemind.isNotEmpty) {
           return NTUTConnectorStatus.PasswordExpiredWarning;
         }
+
         return NTUTConnectorStatus.LoginSuccess;
       } else {
-        String errorMsg = jsonMap["errorMsg"];
+        final errorMsg = jsonMap["errorMsg"];
+
         if (errorMsg.contains("帳號或密碼錯誤")) {
           return NTUTConnectorStatus.AccountPasswordIncorrect;
         } else if (errorMsg.contains("驗證碼")) {
@@ -90,23 +93,24 @@ class NTUTConnector {
     }
   }
 
-  static Future<List<NTUTCalendarJson>> getCalendar(
-      DateTime startTime, DateTime endTime) async {
-    //暫無用到 取得學校行事曆
-    ConnectorParameter parameter;
-    var formatter = DateFormat("yyyy/MM/dd");
-    String startDate = formatter.format(startTime);
-    String endDate = formatter.format(endTime);
+  static Future<List<NTUTCalendarJson>?> getCalendar(
+    DateTime startTime,
+    DateTime endTime,
+  ) async {
+    final formatter = DateFormat("yyyy/MM/dd");
+    final startDate = formatter.format(startTime);
+    final endDate = formatter.format(endTime);
+
     try {
-      Map<String, String> data = {
+      final data = {
         "startDate": startDate,
         "endDate": endDate,
       };
-      parameter = ConnectorParameter(_getCalendarUrl);
-      parameter.data = data;
-      String result = await Connector.getDataByGet(parameter);
-      List<NTUTCalendarJson> calendarList =
-          getNTUTCalendarJsonList(json.decode(result));
+
+      final parameter = ConnectorParameter(_getCalendarUrl)..data = data;
+      final result = await Connector.getDataByGet(parameter);
+      final calendarList = getNTUTCalendarJsonList(json.decode(result));
+
       return calendarList;
     } catch (e, stack) {
       Log.eWithStack(e.toString(), stack);
@@ -114,15 +118,13 @@ class NTUTConnector {
     }
   }
 
-  static Future<APTreeJson> getTree(String arg) async {
-    ConnectorParameter parameter;
+  static Future<APTreeJson?> getTree(String arg) async {
     try {
-      parameter = ConnectorParameter(_getTreeUrl);
-      if (arg != null) {
-        parameter.data = {"apDn": arg};
-      }
-      String result = await Connector.getDataByPost(parameter);
-      APTreeJson apTreeJson = APTreeJson.fromJson(json.decode(result));
+      final data = {"apDn": arg};
+      final parameter = ConnectorParameter(_getTreeUrl)..data = data;
+      final result = await Connector.getDataByPost(parameter);
+      final apTreeJson = APTreeJson.fromJson(json.decode(result));
+
       return apTreeJson;
     } catch (e, stack) {
       Log.eWithStack(e.toString(), stack);
@@ -130,39 +132,37 @@ class NTUTConnector {
     }
   }
 
-  /*
-  Map key
-  url
-  header
-   */
   static Map getUserImage() {
-    Map imageInfo = Map();
-    String userPhoto = Model.instance.getUserInfo().userPhoto;
+    final imageInfo = Map();
+    final userPhoto = Model.instance.getUserInfo().userPhoto;
+
     Log.d("getUserImage");
+
     String url = _getPictureUrl;
-    Map<String, String> data = {"realname": userPhoto};
+    final data = {"realname": userPhoto};
+
     if (userPhoto.isNotEmpty) {
       url =
           Uri.https(Uri.parse(url).host, Uri.parse(url).path, data).toString();
     }
+
     imageInfo["url"] = url;
     imageInfo["header"] = Connector.getLoginHeaders(url);
     return imageInfo;
   }
 
-  static Future<String> changePassword(String password) async {
-    ConnectorParameter parameter;
-    String result;
+  static Future<String?> changePassword(String password) async {
     try {
-      parameter = ConnectorParameter(_changePasswordUrl);
-      String oldPassword = Model.instance.getPassword();
-      parameter.data = {
+      final data = {
         "userPassword": password,
-        "oldPassword": oldPassword,
+        "oldPassword": Model.instance.getPassword(),
         "pwdForceMdy": "profile",
       };
-      result = await Connector.getDataByPost(parameter);
-      var jsonResult = json.decode(result);
+
+      final parameter = ConnectorParameter(_changePasswordUrl)..data = data;
+      final result = await Connector.getDataByPost(parameter);
+      final jsonResult = json.decode(result);
+
       if (jsonResult["success"] == 'true') {
         return "";
       } else {
@@ -174,33 +174,33 @@ class NTUTConnector {
     }
   }
 
-  static Future<List<OrgtreeSearchResult>> orgtreeSearch(String keyword) async {
-    ConnectorParameter parameter;
-    String result;
-    Document tagNode;
-    List<Element> nodes;
-    List<OrgtreeSearchResult> orgtreeSearchList = [];
+  static Future<List<OrgtreeSearchResult>?> orgtreeSearch(
+      String keyword) async {
+    final List<OrgtreeSearchResult> orgtreeSearchList = [];
+
     try {
-      parameter = ConnectorParameter(_orgtreeSearchUrl);
-      parameter.data = {
+      final data = {
         "searchType": '1',
         "excuteFunction": "orgtreeEntryShow",
         "submitFunction": "",
         "searchKeyword": keyword
       };
-      result = await Connector.getDataByGet(parameter);
-      tagNode = parse(result);
-      nodes = tagNode
+      final parameter = ConnectorParameter(_orgtreeSearchUrl)..data = data;
+      final result = await Connector.getDataByGet(parameter);
+
+      final nodes = parse(result)
           .getElementsByClassName("eipOrgTreeDisplay")
           .first
           .getElementsByTagName("div");
-      for (Element node in nodes) {
-        OrgtreeSearchResult r = OrgtreeSearchResult();
+
+      for (final node in nodes) {
+        final r = OrgtreeSearchResult();
         r.id = node.getElementsByTagName("input").last.attributes["value"];
         r.name = node.getElementsByTagName("span").first.text;
         r.msg = node.text;
         orgtreeSearchList.add(r);
       }
+
       return orgtreeSearchList;
     } catch (e, stack) {
       Log.eWithStack(e.toString(), stack);
