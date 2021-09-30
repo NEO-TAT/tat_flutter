8a3cc303a5553be6a20d1435eeaf779ba8c80099
Xanonymous <trusaidlin@gmail.com>
2021-10-01 01:00:25 +0800

    upgrade app config

--- lib/src/config/app_config.dart
+++ lib/src/config/app_config.dart
@@ -1,8 +1,8 @@
 class AppConfig {
   //static bool DEBUG = kDebugMode;
   //App related strings
-  static final String appName = "TAT";
-  static const String method_channel_widget_name = "club.ntut.npc.tat.widget";
-  static const String method_channel_save_name = "club.ntut.npc.tat.save";
-  static bool dioLogDebug = true;
+  static const appName = "TAT";
+  static const method_channel_widget_name = "club.ntut.npc.tat.widget";
+  static const method_channel_save_name = "club.ntut.npc.tat.save";
+  static const dioLogDebug = true;
 }
