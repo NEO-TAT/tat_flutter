d94f3d67d728d6dbe85769318955d2a464ff746f
Xanonymous <trusaidlin@gmail.com>
2021-10-01 00:57:02 +0800

    upgrade score connector

--- lib/src/connector/score_connector.dart
+++ lib/src/connector/score_connector.dart
@@ -21,33 +21,36 @@ class ScoreConnector {
   static const _generalLessonAllScoreUrl = host + "StuQuery/QryLAECourse.jsp";
 
   static Future<ScoreConnectorStatus> login() async {
-    String result;
     try {
       ConnectorParameter parameter;
-      Document tagNode;
-      List<Element> nodes;
-      Map<String, String> data = {
+
+      final data = {
         "apUrl": "https://aps-course.ntut.edu.tw/StuQuery/LoginSID.jsp",
         "apOu": "aa_003_LB",
         "sso": "big5",
         "datetime1": DateTime.now().millisecondsSinceEpoch.toString()
       };
-      parameter = ConnectorParameter(_ssoLoginUrl);
-      parameter.data = data;
-      result = await Connector.getDataByGet(parameter);
-      tagNode = parse(result);
-      nodes = tagNode.getElementsByTagName("input");
-      data = Map();
-      for (Element node in nodes) {
-        String name = node.attributes['name'];
-        String value = node.attributes['value'];
+
+      parameter = ConnectorParameter(_ssoLoginUrl)..data = data;
+
+      final result = await Connector.getDataByGet(parameter);
+      final tagNode = parse(result);
+      final nodes = tagNode.getElementsByTagName("input");
+
+      data.clear();
+
+      for (final node in nodes) {
+        final name = node.attributes['name']!;
+        final value = node.attributes['value']!;
         data[name] = value;
       }
-      String jumpUrl =
-          tagNode.getElementsByTagName("form")[0].attributes["action"];
+
+      final jumpUrl =
+          tagNode.getElementsByTagName("form")[0].attributes["action"]!;
       parameter = ConnectorParameter(jumpUrl);
       parameter.data = data;
       await Connector.getDataByPostResponse(parameter);
+
       return ScoreConnectorStatus.LoginSuccess;
     } catch (e, stack) {
       Log.eWithStack(e.toString(), stack);
@@ -56,37 +59,40 @@ class ScoreConnector {
   }
 
   static String strQ2B(String input) {
-    List<int> newString = [];
+    final List<int> newString = [];
+
     for (int c in input.codeUnits) {
       if (c == 12288) {
         c = 32;
         continue;
       }
+
       if (c > 65280 && c < 65375) {
         c = (c - 65248);
       }
+
       newString.add(c);
     }
     return String.fromCharCodes(newString);
   }
 
-  static Future<List<SemesterCourseScoreJson>> getScoreRankList() async {
-    //取得排名與成績
+  static Future<List<SemesterCourseScoreJson>?> getScoreRankList() async {
     ConnectorParameter parameter;
     String result;
     Document tagNode;
-    Element tableNode, h3Node, scoreNode;
-    List<Element> tableNodes, h3Nodes, scoreNodes, rankNodes;
-    List<SemesterCourseScoreJson> courseScoreList = [];
+    List<Element> rankNodes;
+    final List<SemesterCourseScoreJson> courseScoreList = [];
+
     try {
-      Map<String, String> data = {"format": "-2"};
-      parameter = ConnectorParameter(_scoreAllScoreUrl);
-      parameter.data = data;
-      parameter.charsetName = "big5";
+      final data = {"format": "-2"};
+      parameter = ConnectorParameter(_scoreAllScoreUrl)
+        ..data = data
+        ..charsetName = 'big5';
+
       result = await Connector.getDataByGet(parameter);
       tagNode = parse(result);
-      tableNodes = tagNode.getElementsByTagName("table");
-      h3Nodes = tagNode
+      final tableNodes = tagNode.getElementsByTagName("table");
+      final h3Nodes = tagNode
           .getElementsByTagName("h3")
           .reversed
           .toList()
@@ -94,117 +100,139 @@ class ScoreConnector {
           .toList()
           .reversed
           .toList();
-      //依照學期取得課程資料
+
+      // get course info by semester
       for (int i = 0; i < tableNodes.length; i++) {
-        tableNode = tableNodes[i];
-        h3Node = h3Nodes[i];
-
-        SemesterCourseScoreJson courseScore = SemesterCourseScoreJson();
-
-        SemesterJson semester = SemesterJson();
+        final tableNode = tableNodes[i];
+        final h3Node = h3Nodes[i];
+        final courseScore = SemesterCourseScoreJson();
+        final semester = SemesterJson();
         semester.year = h3Node.text.split(" ")[0];
         semester.semester = h3Node.text.split(" ")[3];
         courseScore.semester = semester;
-        //取得課程名稱與分數
-        scoreNodes = tableNode.getElementsByTagName("tr");
+
+        // get course name and score
+        final scoreNodes = tableNode.getElementsByTagName("tr");
         int scoreEnd = scoreNodes.length - 5;
+
         for (int i = scoreNodes.length - 1; i >= 0; i--) {
-          String text = strQ2B(scoreNodes[i].text).replaceAll("[\n| ]", "");
+          final text = strQ2B(scoreNodes[i].text).replaceAll("[\n| ]", "");
           if (text.contains("This Semester Score")) {
             scoreEnd = i;
             break;
           }
         }
+
         for (int j = 1; j < scoreEnd - 1; j++) {
-          scoreNode = scoreNodes[j];
-          CourseScoreInfoJson score = CourseScoreInfoJson();
+          final scoreNode = scoreNodes[j];
+          final score = CourseScoreInfoJson();
+
           score.courseId = scoreNode
               .getElementsByTagName("th")[0]
               .text
               .replaceAll(RegExp(r"[\s| ]"), "");
+
           score.nameZh = scoreNode
               .getElementsByTagName("th")[2]
               .text
               .replaceAll(RegExp(r"[\s| ]"), "");
+
           score.nameEn = scoreNode
               .getElementsByTagName("th")[3]
               .text
               .replaceAll(RegExp("\n"), "");
+
           score.courseCode = scoreNode
               .getElementsByTagName("th")[4]
               .text
               .replaceAll(RegExp("[\n| ]"), "");
+
           score.credit =
               double.parse(scoreNode.getElementsByTagName("th")[6].text);
+
           score.score = scoreNode
               .getElementsByTagName("th")[7]
               .text
               .replaceAll(RegExp(r"[\s| ]"), "");
+
           courseScore.courseScoreList.add(score);
         }
+
         try {
           courseScore.averageScore = double.parse(
               scoreNodes[scoreNodes.length - 4]
                   .getElementsByTagName("td")[0]
                   .text);
+
           courseScore.performanceScore = double.parse(
               scoreNodes[scoreNodes.length - 3]
                   .getElementsByTagName("td")[0]
                   .text);
+
           courseScore.totalCredit = double.parse(
               scoreNodes[scoreNodes.length - 2]
                   .getElementsByTagName("td")[0]
                   .text);
+
           courseScore.takeCredit = double.parse(
               scoreNodes[scoreNodes.length - 1]
                   .getElementsByTagName("td")[0]
                   .text);
         } catch (e) {
+          // FIXME implement error case.
           continue;
         } finally {
           courseScoreList.add(courseScore);
         }
       }
 
-      parameter = ConnectorParameter(_scoreRankUrl);
-      parameter.data = data;
-      parameter.charsetName = "big5";
+      parameter = ConnectorParameter(_scoreRankUrl)
+        ..data = data
+        ..charsetName = 'big5';
+
       result = await Connector.getDataByGet(parameter);
       tagNode = parse(result);
       rankNodes =
           tagNode.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
       rankNodes =
           rankNodes.getRange(2, rankNodes.length).toList().reversed.toList();
+
       for (int i = 0; i < (rankNodes.length / 3).floor(); i++) {
-        SemesterJson semester = SemesterJson();
-        String semesterString = rankNodes[i * 3 + 2]
+        final semester = SemesterJson();
+        final semesterString = rankNodes[i * 3 + 2]
             .getElementsByTagName("td")[0]
             .innerHtml
             .split("<br>")
             .first;
+
         semester.year = semesterString.split(" ")[0];
         semester.semester = semesterString.split(" ").reversed.toList()[0];
 
-        //取得學期成績排名
-        RankJson rankNow = RankJson();
+        // get semester grades ranking
+        final rankNow = RankJson();
         RankItemJson rankItemCourse = RankItemJson();
         RankItemJson rankItemDepartment = RankItemJson();
+
         rankNow.course = rankItemCourse;
         rankNow.department = rankItemDepartment;
+
         rankItemCourse.rank = double.parse(
             rankNodes[i * 3 + 2].getElementsByTagName("td")[2].text);
         rankItemCourse.total = double.parse(
             rankNodes[i * 3 + 2].getElementsByTagName("td")[3].text);
-        String percentage = rankNodes[i * 3 + 2]
+
+        final percentage = rankNodes[i * 3 + 2]
             .getElementsByTagName("td")[4]
             .text
             .replaceAll(RegExp("[%|\n]"), "");
+
         try {
           rankItemCourse.percentage = double.parse(percentage);
         } catch (e) {
           rankItemCourse.percentage =
               double.parse(percentage.replaceAll("%", ""));
         }
+
         rankItemDepartment.rank =
             double.parse(rankNodes[i * 3].getElementsByTagName("td")[1].text);
         rankItemDepartment.total =
@@ -214,12 +242,14 @@ class ScoreConnector {
             .text
             .replaceAll(RegExp(r"[%|\s]"), ""));
 
-        //取得歷年成績排名
-        RankJson rankHistory = RankJson();
+        // get rankings over the years
+        final rankHistory = RankJson();
         rankItemCourse = RankItemJson();
         rankItemDepartment = RankItemJson();
+
         rankHistory.course = rankItemCourse;
         rankHistory.department = rankItemDepartment;
+
         rankItemCourse.rank = double.parse(
             rankNodes[i * 3 + 2].getElementsByTagName("td")[5].text);
         rankItemCourse.total = double.parse(
@@ -228,6 +258,7 @@ class ScoreConnector {
             .getElementsByTagName("td")[7]
             .text
             .replaceAll("%", ""));
+
         rankItemDepartment.rank =
             double.parse(rankNodes[i * 3].getElementsByTagName("td")[4].text);
         rankItemDepartment.total =
@@ -245,6 +276,7 @@ class ScoreConnector {
           }
         }
       }
+
       return courseScoreList;
     } catch (e, stack) {
       Log.eWithStack(e.toString(), stack);
@@ -252,24 +284,24 @@ class ScoreConnector {
     }
   }
 
-  static Future<List<String>> getCoreGeneralLesson() async {
-    ConnectorParameter parameter;
-    String result;
-    Document tagNode;
-    Element node;
-    List<Element> nodes;
-    List<String> coreGeneralLessonList = [];
+  static Future<List<String>?> getCoreGeneralLesson() async {
+    final List<String> coreGeneralLessonList = [];
+
     try {
-      parameter = ConnectorParameter(_generalLessonAllScoreUrl);
-      parameter.charsetName = "big5";
-      result = await Connector.getDataByGet(parameter);
-      tagNode = parse(result);
-      node = tagNode.getElementsByTagName("tbody").first;
-      nodes = node.getElementsByTagName("tr");
+      final parameter = ConnectorParameter(_generalLessonAllScoreUrl)
+        ..charsetName = 'big5';
+
+      final result = await Connector.getDataByGet(parameter);
+      final nodes = parse(result)
+          .getElementsByTagName("tbody")
+          .first
+          .getElementsByTagName("tr");
+
       for (int i = 2; i < nodes.length; i++) {
-        node = nodes[i];
+        final node = nodes[i];
         if (node.innerHtml.contains("＊")) {
           String name;
+
           if (node
               .getElementsByTagName("td")[0]
               .attributes
