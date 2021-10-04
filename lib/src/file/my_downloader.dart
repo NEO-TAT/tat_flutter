980b016eab344cb1a2392e9c777e7a4c3598845f
Xanonymous <trusaidlin@gmail.com>
2021-10-05 00:46:44 +0800

    upgrade my downloader

--- lib/src/file/my_downloader.dart
+++ lib/src/file/my_downloader.dart
@@ -1,25 +1,30 @@
 import 'dart:isolate';
 import 'dart:ui';
 
-import 'package:tat/debug/log/log.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:tat/debug/log/log.dart';
 
 class MyDownloader {
   static Map<String, Function> _callBackMap = Map();
   static bool isInit = false;
-  static ReceivePort _port = ReceivePort();
+  static final _port = ReceivePort();
 
   static init() async {
     if (!isInit) {
       await FlutterDownloader.initialize();
+
       IsolateNameServer.registerPortWithName(
-          _port.sendPort, 'downloader_send_port');
+        _port.sendPort,
+        'downloader_send_port',
+      );
+
       _port.listen((dynamic data) {
         String id = data[0];
         DownloadTaskStatus status = data[1];
         int progress = data[2];
         _downloadListen(id, status, progress);
       });
+
       FlutterDownloader.registerCallback(downloadCallback);
       isInit = true;
     }
@@ -28,19 +33,18 @@ class MyDownloader {
   static _downloadListen(String id, DownloadTaskStatus status, int progress) {
     if (status == DownloadTaskStatus.complete) {
       Log.d("$id complete");
-      List<String> keyList = _callBackMap.keys.toList();
+      final keyList = _callBackMap.keys.toList();
+
       for (int i = 0; i < keyList.length; i++) {
-        String mapId = keyList[i];
+        final mapId = keyList[i];
         if (mapId == id) {
           Log.d("$id find callback");
-          _callBackMap[mapId]();
-          //Function.apply(_callBackMap[mapId] , []);
+          _callBackMap[mapId]!();
           _callBackMap.remove(mapId);
         }
       }
     } else if (status == DownloadTaskStatus.failed) {
       Log.d("$id fail");
-      //FlutterDownloader.retry(taskId: id);
     }
   }
 
@@ -50,9 +54,11 @@ class MyDownloader {
   }
 
   static void downloadCallback(
-      String id, DownloadTaskStatus status, int progress) {
-    final SendPort send =
-        IsolateNameServer.lookupPortByName('downloader_send_port');
+    String id,
+    DownloadTaskStatus status,
+    int progress,
+  ) {
+    final send = IsolateNameServer.lookupPortByName('downloader_send_port')!;
     send.send([id, status, progress]);
   }
 
