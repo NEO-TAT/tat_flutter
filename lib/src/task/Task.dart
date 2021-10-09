8aca4eeb0518d5ac69a08395cf79c4d5e8737ebe
Xanonymous <trusaidlin@gmail.com>
2021-10-09 16:03:06 +0800

    make Task to be late final

--- lib/src/task/Task.dart
+++ lib/src/task/Task.dart
@@ -2,7 +2,7 @@ enum TaskStatus { Success, GiveUp, Restart }
 
 abstract class Task<T> {
   late final T result;
-  final String name;
+  late final String name;
 
   Task(this.name, {Map<String, dynamic>? arg});
 
