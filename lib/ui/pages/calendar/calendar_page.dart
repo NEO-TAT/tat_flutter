791f04067cccd8887496f21f06f251ed7241ce9d
Xanonymous <trusaidlin@gmail.com>
2021-10-20 11:44:48 +0800

    upgrade calendar page

--- lib/ui/pages/calendar/calendar_page.dart
+++ lib/ui/pages/calendar/calendar_page.dart
@@ -1,119 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:tat/src/R.dart';
 import 'package:tat/src/model/ntut/ntut_calendar_json.dart';
 import 'package:tat/src/task/ntut/ntut_calendar_task.dart';
 import 'package:tat/src/task/task_flow.dart';
 import 'package:tat/src/util/language_utils.dart';
-import 'package:tat/ui/pages/calendar/calendar_detail_dialog.dart';
-import 'package:get/get.dart';
-import 'package:table_calendar/table_calendar.dart';
+
+import 'calendar_detail_dialog.dart';
+
+final kNow = DateTime.now();
+final kFirstDay = DateTime(kNow.year, kNow.month - 3, kNow.day);
+final kLastDay = DateTime(kNow.year, kNow.month + 3, kNow.day);
 
 class CalendarPage extends StatefulWidget {
   @override
   _CalendarPageState createState() => _CalendarPageState();
 }
 
-// Example holidays
-final Map<DateTime, List> _holidays = {
-  /*
-  DateTime(2019, 1, 1): ['New Year\'s Day'],
-  DateTime(2019, 1, 6): ['Epiphany'],
-  DateTime(2019, 2, 14): ['Valentine\'s Day'],
-  DateTime(2019, 4, 21): ['Easter Sunday'],
-  DateTime(2019, 4, 22): ['Easter Monday'],
-
-   */
-};
-
-class _CalendarPageState extends State<CalendarPage>
-    with TickerProviderStateMixin {
-  Map<DateTime, List> _events = Map();
-  List _selectedEvents;
-  AnimationController _animationController;
-  CalendarController _calendarController;
+class _CalendarPageState extends State<CalendarPage> {
+  late List<NTUTCalendarJson> _selectedEvents = [];
+  late Map<DateTime, List<NTUTCalendarJson>> _events = Map();
+  late CalendarFormat _calendarFormat = CalendarFormat.month;
+  late RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOff;
+  late DateTime _focusedDay = DateTime.now();
+  late DateTime _selectedDay;
+  late DateTime? _rangeStart;
+  late DateTime? _rangeEnd;
 
   @override
   void initState() {
     super.initState();
-    _selectedEvents = [];
-    _calendarController = CalendarController();
-
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 400),
-    );
-
-    _animationController.forward();
+    _selectedDay = _focusedDay;
+    _selectedEvents.clear();
     _addEvent();
   }
 
   void _addEvent() async {
-    var _selectedDay = DateTime.now();
-    _selectedDay = _selectedDay.add(Duration(hours: 8)); //to TW time
+    final _selectedDay = DateTime.now()..add(Duration(hours: 8));
     print(_selectedDay.toIso8601String());
     await _getEvent(_selectedDay);
-    for (DateTime time in _events.keys) {
-      //顯示當天事件
-      int diffDays = _selectedDay.difference(time).inDays;
-      bool isSame = (diffDays == 0);
+  }
+
+  void _selectEvent() {
+    for (final time in _events.keys) {
+      final diffDays = _selectedDay.difference(time).inDays;
+      final isSame = (diffDays == 0);
       if (isSame) {
-        _onDaySelected(_selectedDay, _events[time], null);
+        setState(() {
+          _selectedEvents = _events[time]!;
+        });
         break;
       }
     }
   }
 
   Future<void> _getEvent(DateTime time) async {
-    DateTime startTime = DateTime(time.year, time.month, 1);
-    DateTime endTime = DateTime(time.year, time.month + 1, 1);
-    List<NTUTCalendarJson> eventNTUTs;
-
-    TaskFlow taskFlow = TaskFlow();
-    var calendarTask = NTUTCalendarTask(startTime, endTime);
+    final startTime = DateTime(time.year, time.month, 1);
+    final endTime = DateTime(time.year, time.month + 1, 1);
+    final taskFlow = TaskFlow();
+    final calendarTask = NTUTCalendarTask(startTime, endTime);
     taskFlow.addTask(calendarTask);
     _events = Map();
     if (await taskFlow.start()) {
-      eventNTUTs = calendarTask.result;
+      final eventNTUTs = calendarTask.result;
       _events = Map();
       for (int i = 0; i < eventNTUTs.length; i++) {
-        NTUTCalendarJson eventNTUT = eventNTUTs[i];
-        for (var time = eventNTUT.startTime;
+        final eventNTUT = eventNTUTs[i];
+        for (DateTime time = DateTime.utc(
+          eventNTUT.startTime.year,
+          eventNTUT.startTime.month,
+          eventNTUT.startTime.day,
+        );
             time.isBefore(eventNTUT.endTime);
-            time = time.add(Duration(days: 1)))
+            time = time.add(Duration(days: 1))) {
           if (_events.containsKey(time)) {
-            _events[time].add(eventNTUT);
+            _events[time]!.add(eventNTUT);
           } else {
             _events[time] = [eventNTUT];
           }
+        }
       }
     }
     setState(() {
-      _selectedEvents = [];
+      _selectedDay = time;
     });
+    _selectEvent();
   }
 
-  @override
-  void dispose() {
-    _animationController.dispose();
-    _calendarController.dispose();
-    super.dispose();
+  List<NTUTCalendarJson> _getEventsForDay(DateTime day) {
+    return _events[day] ?? [];
   }
 
-  void _onDaySelected(DateTime day, List events, List holidays) {
-    print('CALLBACK: _onDaySelected');
-    setState(() {
-      _selectedEvents = events;
-    });
-  }
-
-  static var lastUpdateTime;
-
-  void _onVisibleDaysChanged(
-      DateTime first, DateTime last, CalendarFormat format) async {
-    print('CALLBACK: _onVisibleDaysChanged');
-    if (lastUpdateTime != first) {
-      lastUpdateTime = first;
-      await _getEvent(first);
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_focusedDay, focusedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _rangeStart = null; // Important to clean those
+        _rangeEnd = null;
+        _rangeSelectionMode = RangeSelectionMode.toggledOff;
+        _selectedEvents = _getEventsForDay(focusedDay);
+      });
     }
   }
 
@@ -124,166 +112,70 @@ class _CalendarPageState extends State<CalendarPage>
         title: Text(R.current.calendar),
       ),
       body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          // Switch out 2 lines below to play with TableCalendar's settings
-          //-----------------------
-          _buildTableCalendar(),
-          // _buildTableCalendarWithBuilders(),
-          const SizedBox(height: 8.0),
-          //_buildButtons(),
+        children: [
+          TableCalendar<NTUTCalendarJson>(
+            locale: (LanguageUtils.getLangIndex() == LangEnum.zh)
+                ? "zh_CN"
+                : "en_US",
+            availableCalendarFormats: {
+              CalendarFormat.month: 'Month',
+            },
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            rangeStartDay: _rangeStart,
+            rangeEndDay: _rangeEnd,
+            calendarFormat: _calendarFormat,
+            rangeSelectionMode: _rangeSelectionMode,
+            eventLoader: _getEventsForDay,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            daysOfWeekStyle: DaysOfWeekStyle(
+              weekdayStyle: TextStyle(
+                color: Color(0xFF4F4F4F),
+              ),
+              weekendStyle: TextStyle(
+                color: Colors.deepOrange,
+              ),
+            ),
+            calendarStyle: CalendarStyle(
+              // Use `CalendarStyle` to customize the UI
+              outsideDaysVisible: false,
+              weekendTextStyle: TextStyle(
+                color: Colors.deepOrange,
+              ),
+              markerDecoration: BoxDecoration(
+                color: Colors.brown[700],
+                shape: BoxShape.circle,
+              ),
+              selectedDecoration: BoxDecoration(
+                color: Colors.deepOrange[300],
+                shape: BoxShape.circle,
+              ),
+              todayDecoration: BoxDecoration(
+                color: Colors.deepOrange[200],
+                shape: BoxShape.circle,
+              ),
+            ),
+            onDaySelected: _onDaySelected,
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+              _getEvent(_focusedDay);
+            },
+          ),
           const SizedBox(height: 8.0),
           Expanded(child: _buildEventList()),
         ],
       ),
     );
   }
-
-  // Simple TableCalendar configuration (using Styles)
-  Widget _buildTableCalendar() {
-    return TableCalendar(
-      locale: (LanguageUtils.getLangIndex() == LangEnum.zh) ? "zh_CN" : "en_US",
-      calendarController: _calendarController,
-      events: _events,
-      holidays: _holidays,
-      startingDayOfWeek: StartingDayOfWeek.monday,
-      calendarStyle: CalendarStyle(
-        selectedColor: Colors.deepOrange[400],
-        todayColor: Colors.deepOrange[200],
-        markersColor: Colors.brown[700],
-        outsideDaysVisible: false,
-      ),
-      headerStyle: HeaderStyle(
-        formatButtonTextStyle:
-            TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
-        formatButtonDecoration: BoxDecoration(
-          color: Colors.deepOrange[400],
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-      ),
-      onDaySelected: _onDaySelected,
-      onVisibleDaysChanged: _onVisibleDaysChanged,
-    );
-  }
-
-  /*
-  // More advanced TableCalendar configuration (using Builders & Styles)
-  Widget _buildTableCalendarWithBuilders() {
-    return TableCalendar(
-      locale: 'pl_PL',
-      calendarController: _calendarController,
-      events: _events,
-      holidays: _holidays,
-      initialCalendarFormat: CalendarFormat.month,
-      formatAnimation: FormatAnimation.slide,
-      startingDayOfWeek: StartingDayOfWeek.sunday,
-      availableGestures: AvailableGestures.all,
-      availableCalendarFormats: const {
-        CalendarFormat.month: '',
-        CalendarFormat.week: '',
-      },
-      calendarStyle: CalendarStyle(
-        outsideDaysVisible: false,
-        weekendStyle: TextStyle().copyWith(color: Colors.blue[800]),
-        holidayStyle: TextStyle().copyWith(color: Colors.blue[800]),
-      ),
-      daysOfWeekStyle: DaysOfWeekStyle(
-        weekendStyle: TextStyle().copyWith(color: Colors.blue[600]),
-      ),
-      headerStyle: HeaderStyle(
-        centerHeaderTitle: true,
-        formatButtonVisible: false,
-      ),
-      builders: CalendarBuilders(
-        selectedDayBuilder: (context, date, _) {
-          return FadeTransition(
-            opacity: Tween(begin: 0.0, end: 1.0).animate(_animationController),
-            child: Container(
-              margin: const EdgeInsets.all(4.0),
-              padding: const EdgeInsets.only(top: 5.0, left: 6.0),
-              color: Colors.deepOrange[300],
-              width: 100,
-              height: 100,
-              child: Text(
-                '${date.day}',
-                style: TextStyle().copyWith(fontSize: 16.0),
-              ),
-            ),
-          );
-        },
-        todayDayBuilder: (context, date, _) {
-          return Container(
-            margin: const EdgeInsets.all(4.0),
-            padding: const EdgeInsets.only(top: 5.0, left: 6.0),
-            color: Colors.amber[400],
-            width: 100,
-            height: 100,
-            child: Text(
-              '${date.day}',
-              style: TextStyle().copyWith(fontSize: 16.0),
-            ),
-          );
-        },
-        markersBuilder: (context, date, events, holidays) {
-          final children = <Widget>[];
-
-          if (events.isNotEmpty) {
-            children.add(
-              Positioned(
-                right: 1,
-                bottom: 1,
-                child: _buildEventsMarker(date, events),
-              ),
-            );
-          }
-
-          if (holidays.isNotEmpty) {
-            children.add(
-              Positioned(
-                right: -2,
-                top: -2,
-                child: _buildHolidaysMarker(),
-              ),
-            );
-          }
-
-          return children;
-        },
-      ),
-      onDaySelected: (date, events) {
-        _onDaySelected(date, events);
-        _animationController.forward(from: 0.0);
-      },
-      onVisibleDaysChanged: _onVisibleDaysChanged,
-    );
-  }
-
-  Widget _buildEventsMarker(DateTime date, List events) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      decoration: BoxDecoration(
-        shape: BoxShape.rectangle,
-        color: _calendarController.isSelected(date)
-            ? Colors.brown[500]
-            : _calendarController.isToday(date)
-                ? Colors.brown[300]
-                : Colors.blue[400],
-      ),
-      width: 16.0,
-      height: 16.0,
-      child: Center(
-        child: Text(
-          '${events.length}',
-          style: TextStyle().copyWith(
-            color: Colors.white,
-            fontSize: 12.0,
-          ),
-        ),
-      ),
-    );
-  }
-
-   */
 
   Widget _buildEventList() {
     return ListView(
@@ -300,7 +192,6 @@ class _CalendarPageState extends State<CalendarPage>
                     onTap: () {
                       Get.dialog(
                         CalendarDetailDialog(calendarDetail: event),
-                        useRootNavigator: false,
                         barrierDismissible: true,
                       );
                     }),
