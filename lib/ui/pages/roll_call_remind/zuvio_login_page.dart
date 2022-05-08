f601859a637c3cb9bf302dd978ec9fb8edf93258
Xanonymous <trusaidlin@gmail.com>
2022-05-08 08:54:03 +0800

    feat: extract login handler call and tidy-up widget and add form validates

--- lib/ui/pages/roll_call_remind/zuvio_login_page.dart
+++ lib/ui/pages/roll_call_remind/zuvio_login_page.dart
@@ -1,31 +1,69 @@
 // TODO: remove sdk version selector after migrating to null-safety.
 // @dart=2.16
 
-import 'dart:async';
 import 'dart:math';
 
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/src/R.dart';
 import 'package:flutter_app/src/controllers/zuvio_auth_controller.dart';
 import 'package:get/get.dart';
 
-typedef LoginHandler = FutureOr<void> Function({String username, String password});
+/// A function to be invoked after logged in successfully.
+typedef LoginSuccessAction = void Function();
+
+/// A function to be invoked after login canceled.
+typedef CancelLoginAction = void Function();
+
+/// A function to called when content of a [TextFormField] or [TextField] changed.
+typedef TextFieldValidator = String? Function(String?);
 
 class ZuvioLoginPage extends StatelessWidget {
   ZuvioLoginPage({
     Key? key,
-    LoginHandler? loginHandler,
-    void Function()? onPageClose,
-  })  : _loginHandler = loginHandler,
-        _onPageClose = onPageClose,
-        _userNameInputBoxController = TextEditingController(),
-        _passwordInputBoxController = TextEditingController(),
+    LoginSuccessAction? onLoginSuccess,
+    CancelLoginAction? onPageClose,
+  })  : _onPageClose = onPageClose,
+        _onLoginSuccess = onLoginSuccess,
         super(key: key);
 
-  final LoginHandler? _loginHandler;
-  final void Function()? _onPageClose;
-  final TextEditingController _userNameInputBoxController;
-  final TextEditingController _passwordInputBoxController;
+  final LoginSuccessAction? _onLoginSuccess;
+  final CancelLoginAction? _onPageClose;
+
+  final TextEditingController _userNameInputBoxController = TextEditingController();
+  final TextEditingController _passwordInputBoxController = TextEditingController();
+
+  void _onLoginPressed() {
+    final username = _userNameInputBoxController.text.trim();
+    final password = _passwordInputBoxController.text;
+
+    ZAuthController.to.login(username, password);
+  }
+
+  void _handleLoginCallBack() {
+    if (ZAuthController.to.isLoggedIntoZuvio()) {
+      _onLoginSuccess?.call();
+    } else {
+      _passwordInputBoxController.clear();
+    }
+  }
+
+  String? _emailValidator(String? rawData) {
+    if (rawData == null) {
+      return R.current.accountNull;
+    }
+
+    final isValid = EmailValidator.validate(rawData.trim());
+    return isValid ? null : R.current.error;
+  }
+
+  String? _passwordValidator(String? rawData) {
+    if (rawData == null) {
+      return R.current.passwordNull;
+    }
+
+    return rawData.isNotEmpty ? null : R.current.error;
+  }
 
   Widget _buildLoginButton({required bool enabled}) => Padding(
         padding: EdgeInsets.symmetric(vertical: 12, horizontal: 0),
@@ -68,6 +106,25 @@ class ZuvioLoginPage extends StatelessWidget {
         ],
       );
 
+  Widget _buildEmailTextField({required bool enabled}) => _InputBox(
+        controller: _userNameInputBoxController,
+        keyboardType: TextInputType.emailAddress,
+        icon: Icons.account_circle_rounded,
+        hintText: R.current.account,
+        enabled: enabled,
+        validator: _emailValidator,
+      );
+
+  Widget _buildPasswordTextField({required bool enabled}) => _InputBox(
+        controller: _passwordInputBoxController,
+        keyboardType: TextInputType.visiblePassword,
+        icon: Icons.lock,
+        hintText: R.current.password,
+        obscure: true,
+        enabled: enabled,
+        validator: _passwordValidator,
+      );
+
   Widget get _loginBox => Padding(
         padding: const EdgeInsets.all(18),
         child: Container(
@@ -82,30 +139,23 @@ class ZuvioLoginPage extends StatelessWidget {
                     data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
                     child: ConstrainedBox(
                       constraints: BoxConstraints.tightFor(
-                        height: min(300, constraints.maxHeight),
+                        height: min(330, constraints.maxHeight),
                       ),
                       child: GetBuilder<ZAuthController>(
-                        builder: (controller) => Column(
-                          children: [
-                            _boxTitle,
-                            _InputBox(
-                              controller: _userNameInputBoxController,
-                              keyboardType: TextInputType.emailAddress,
-                              icon: Icons.account_circle_rounded,
-                              hintText: R.current.account,
-                              enabled: controller.isInputBoxesEnabled,
-                            ),
-                            _InputBox(
-                              controller: _passwordInputBoxController,
-                              keyboardType: TextInputType.visiblePassword,
-                              icon: Icons.lock,
-                              hintText: R.current.password,
-                              obscure: true,
-                              enabled: controller.isInputBoxesEnabled,
-                            ),
-                            _buildLoginButton(enabled: controller.isLoginBtnEnabled),
-                          ],
-                        ),
+                        builder: (controller) {
+                          // Since the [didChangeDependencies] of the [GetBuilderState] won't be called,
+                          // we need to put the logics needs to be done after [build] finished here.
+                          WidgetsBinding.instance?.addPostFrameCallback((_) => _handleLoginCallBack());
+
+                          return Column(
+                            children: [
+                              _boxTitle,
+                              _buildEmailTextField(enabled: controller.isInputBoxesEnabled),
+                              _buildPasswordTextField(enabled: controller.isInputBoxesEnabled),
+                              _buildLoginButton(enabled: controller.isLoginBtnEnabled),
+                            ],
+                          );
+                        },
                       ),
                     ),
                   ),
@@ -145,18 +195,6 @@ class ZuvioLoginPage extends StatelessWidget {
           ),
         ),
       );
-
-  void _onLoginPressed() {
-    if (_loginHandler == null) return;
-
-    final username = _userNameInputBoxController.text.trim();
-    final password = _passwordInputBoxController.text;
-
-    _loginHandler?.call(
-      username: username,
-      password: password,
-    );
-  }
 }
 
 class _InputBox extends StatelessWidget {
@@ -168,12 +206,14 @@ class _InputBox extends StatelessWidget {
     required String hintText,
     bool obscure = false,
     bool enabled = true,
+    TextFieldValidator? validator,
   })  : _controller = controller,
         _keyboardType = keyboardType,
         _icon = icon,
         _hintText = hintText,
         _obscure = obscure,
         _enabled = enabled,
+        _validator = validator,
         super(key: key);
 
   final TextEditingController _controller;
@@ -182,6 +222,7 @@ class _InputBox extends StatelessWidget {
   final String _hintText;
   final bool _obscure;
   final bool _enabled;
+  final TextFieldValidator? _validator;
 
   OutlineInputBorder _buildBorder({
     required Color color,
@@ -200,6 +241,7 @@ class _InputBox extends StatelessWidget {
           keyboardType: _keyboardType,
           obscureText: _obscure,
           enabled: _enabled,
+          validator: _validator,
           decoration: InputDecoration(
             fillColor: Color(0xFFEAE5D2),
             filled: true,
