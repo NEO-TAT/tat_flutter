08c84dbdde84baa730c76f794cd709c7cb51dbce
Xanonymous <trusaidlin@gmail.com>
2022-06-23 04:45:25 +0800

    feat: init schdeule monitor card widget

---
+++ lib/ui/pages/roll_call_remind/scheduled_roll_call_monitor_card_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ScheduledRollCallMonitorCard extends StatelessWidget {
+  const ScheduledRollCallMonitorCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
