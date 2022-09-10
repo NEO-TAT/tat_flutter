fcc13b97d2c6c47b596e1cf84044c0ea5334da75
Xanonymous <trusaidlin@gmail.com>
2022-09-10 22:12:28 +0800

    feat: add remote config for the incognito guide page url

--- lib/src/config/app_config.dart
+++ lib/src/config/app_config.dart
@@ -4,4 +4,7 @@ class AppConfig {
   static const appName = "TAT";
   static const methodChannelName = "tat/global";
   static final zuvioRollCallFeatureEnabled = RemoteConfigUtil.getFeatureToggleZuvioRollCallFlag();
+
+  // Only for android use.
+  static final androidChromeIncognitoFlagSetupPageUrl = RemoteConfigUtil.getAndroidIncognitoSetupGuidePageUrl();
 }
