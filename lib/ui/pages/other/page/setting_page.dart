85680ff2d93ef064cdbcbbc38c6eaa38bcd75af2
Xanonymous <trusaidlin@gmail.com>
2022-09-10 22:11:20 +0800

    feat: add new setting item for opening webview to the incognito guide

--- lib/ui/pages/other/page/setting_page.dart
+++ lib/ui/pages/other/page/setting_page.dart
@@ -1,8 +1,9 @@
-// TODO: remove sdk version selector after migrating to null-safety.
-// @dart=2.10
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_app/src/config/app_config.dart';
 import 'package:flutter_app/src/config/app_themes.dart';
 import 'package:flutter_app/src/file/file_store.dart';
 import 'package:flutter_app/src/providers/app_provider.dart';
@@ -10,6 +11,7 @@ import 'package:flutter_app/src/r.dart';
 import 'package:flutter_app/src/store/local_storage.dart';
 import 'package:flutter_app/src/util/language_util.dart';
 import 'package:flutter_app/ui/other/list_view_animator.dart';
+import 'package:flutter_app/ui/other/route_utils.dart';
 import "package:flutter_feather_icons/flutter_feather_icons.dart";
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
@@ -17,14 +19,14 @@ import 'package:provider/provider.dart';
 class SettingPage extends StatefulWidget {
   final PageController pageController;
 
-  const SettingPage(this.pageController, {Key key}) : super(key: key);
+  const SettingPage(this.pageController, {super.key});
 
   @override
   State<SettingPage> createState() => _SettingPageState();
 }
 
 class _SettingPageState extends State<SettingPage> {
-  String downloadPath;
+  String? downloadPath;
 
   @override
   void initState() {
@@ -42,13 +44,19 @@ class _SettingPageState extends State<SettingPage> {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> listViewData = [];
+    final List<Widget> listViewData = [];
+
     listViewData.add(_buildLanguageSetting());
-    if (Platform.isAndroid) {
-      listViewData.add(_buildOpenExternalVideoSetting());
-    }
     listViewData.add(_buildLoadIPlusNewsSetting());
     listViewData.add(_buildDarkModeSetting());
+
+    if (Platform.isAndroid) {
+      listViewData.add(_buildOpenExternalVideoSetting());
+
+      // TODO: ensure remote config fetched successfully then enable this line.
+      // listViewData.add(_buildAndroidAllowPrivateBrowserGuide());
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(R.current.setting),
@@ -56,8 +64,7 @@ class _SettingPageState extends State<SettingPage> {
       body: ListView.separated(
         itemCount: listViewData.length,
         itemBuilder: (context, index) {
-          Widget widget;
-          widget = listViewData[index];
+          final widget = listViewData[index];
           return Container(
             padding: const EdgeInsets.only(top: 5, left: 20, right: 20),
             child: WidgetAnimator(widget),
@@ -82,7 +89,7 @@ class _SettingPageState extends State<SettingPage> {
       contentPadding: const EdgeInsets.all(0),
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
+        children: [
           Text(
             R.current.languageSwitch,
             style: textTitle,
@@ -96,7 +103,7 @@ class _SettingPageState extends State<SettingPage> {
       value: (LanguageUtil.getLangIndex() == LangEnum.en),
       onChanged: (value) {
         setState(() {
-          int langIndex = 1 - LanguageUtil.getLangIndex().index;
+          final int langIndex = 1 - LanguageUtil.getLangIndex().index;
           LanguageUtil.setLangByIndex(LangEnum.values.toList()[langIndex]).then((_) {
             widget.pageController.jumpToPage(0);
             Get.back();
@@ -111,7 +118,7 @@ class _SettingPageState extends State<SettingPage> {
         ? SwitchListTile.adaptive(
             contentPadding: const EdgeInsets.all(0),
             title: Row(
-              children: <Widget>[
+              children: [
                 Text(
                   R.current.darkMode,
                   style: textTitle,
@@ -141,7 +148,7 @@ class _SettingPageState extends State<SettingPage> {
       contentPadding: const EdgeInsets.all(0),
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
+        children: [
           Text(
             R.current.checkIPlusNew,
             style: textTitle,
@@ -163,7 +170,7 @@ class _SettingPageState extends State<SettingPage> {
       contentPadding: const EdgeInsets.all(0),
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
+        children: [
           Text(
             R.current.openExternalVideo,
             style: textTitle,
@@ -183,4 +190,35 @@ class _SettingPageState extends State<SettingPage> {
       },
     );
   }
+
+  // TODO: ensure remote config fetched successfully then enable this line.
+  // ignore: unused_element
+  Widget _buildAndroidAllowPrivateBrowserGuide() => ListTile(
+        contentPadding: const EdgeInsets.all(0),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              R.current.androidPrivateBrowseGuideTitle,
+              style: textTitle,
+            ),
+            Text(
+              R.current.androidPrivateBrowseGuideSubTitle,
+              style: textBody,
+            ),
+          ],
+        ),
+        onTap: () async {
+          final guidePageUrl = await AppConfig.androidChromeIncognitoFlagSetupPageUrl;
+          if (guidePageUrl.isEmpty) {
+            return;
+          }
+
+          final parsedGuidePageUrl = Uri.tryParse(guidePageUrl);
+
+          if (parsedGuidePageUrl != null) {
+            RouteUtils.toWebViewPage(initialUrl: parsedGuidePageUrl);
+          }
+        },
+      );
 }
