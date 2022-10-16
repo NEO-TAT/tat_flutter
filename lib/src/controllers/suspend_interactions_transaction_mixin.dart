1bb3a9330e5e84740b1c8e67ae651f1c795667c1
Xanonymous <trusaidlin@gmail.com>
2022-10-16 22:38:03 +0800

    feat: add suspend ineraction transaction mixin

---
+++ lib/src/controllers/suspend_interactions_transaction_mixin.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:get/get.dart';
+
+typedef UISuspendedTransaction<T> = FutureOr<T> Function();
+
+/// A mixin to suspend interactions with UI.
+mixin SuspendInteractionsTransaction on GetxController {
+  /// Suspend interactions with UI.
+  void suspendUIInteractions();
+
+  /// Resume interactions with UI.
+  void resumeUIInteractions();
+
+  /// Execute a transaction that suspends interactions with UI.
+  FutureOr<T> suspendInteractionsTransaction<T>({required UISuspendedTransaction<T> transaction}) async {
+    // Suspend interactions with UI.
+    suspendUIInteractions();
+    update();
+
+    // Execute the transaction.
+    final result = await transaction();
+
+    // Resume interactions with UI.
+    resumeUIInteractions();
+    update();
+
+    return result;
+  }
+}
