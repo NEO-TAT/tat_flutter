02698f43a2e8781fab9ed7742dbd991e9a3f666d
ggjj321 <10370324lsjh@gmail.com>
2022-10-30 22:56:26 +0800

    add take picture options

--- lib/ui/pages/note_camera/note_camera.dart
+++ lib/ui/pages/note_camera/note_camera.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -20,6 +22,10 @@ class _NoteCameraState extends State<NoteCamera> with WidgetsBindingObserver {
   final PictureStorage _pictureStorage;
 
   double _zoom = 1.0;
+
+  XFile? image;
+
+  final _isPressShutter = false.obs;
 
   _NoteCameraState() : _pictureStorage = PictureStorage();
 
@@ -56,15 +62,35 @@ class _NoteCameraState extends State<NoteCamera> with WidgetsBindingObserver {
     }
   }
 
-  void takePicture() async {
-    final image = await controller?.takePicture();
+  void _pressShutter() async{
+    image = await controller?.takePicture();
+    controller?.pausePreview();
+    _isPressShutter.toggle();
+  }
+
+  void _deletePicture(){
+    String? path = image?.path;
+    if (path != null) {
+      final imgFile = File(path);
+      imgFile.delete();
+    }
+    _backToCamera();
+  }
+
+  void _takePicture() async {
     String? path = image?.path;
     if (path != null) {
       _pictureStorage.takePictureToStorage(widget.courseId, path);
     }
+    _backToCamera();
   }
 
-  void adjustZoom(scaleStartDetails) {
+  void _backToCamera(){
+    controller?.resumePreview();
+    _isPressShutter.toggle();
+  }
+
+  void _adjustZoom(scaleStartDetails) {
     double newZoom = 0.0;
     if (scaleStartDetails.scale > 1.0) {
       newZoom = _zoom + 0.01;
@@ -111,7 +137,7 @@ class _NoteCameraState extends State<NoteCamera> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     return GestureDetector(
       behavior: HitTestBehavior.translucent,
-      onScaleUpdate: (scaleStartDetails) => adjustZoom(scaleStartDetails),
+      onScaleUpdate: (scaleStartDetails) => _adjustZoom(scaleStartDetails),
       child: _isCameraInitialized
           ? CameraPreview(
               controller!,
@@ -119,16 +145,36 @@ class _NoteCameraState extends State<NoteCamera> with WidgetsBindingObserver {
                 alignment: Alignment.bottomCenter,
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  child: ClipOval(
-                    child: Material(
-                      color: Colors.white,
-                      child: InkWell(
-                        splashColor: Colors.grey[300]?.withOpacity(0.7),
-                        splashFactory: InkRipple.splashFactory,
-                        onTap: () => takePicture(),
-                        child: const SizedBox(width: 56, height: 56),
-                      ),
-                    ),
+                  child: Obx(
+                    () => _isPressShutter.isFalse
+                        ? ClipOval(
+                            child: Material(
+                              color: Colors.white,
+                              child: InkWell(
+                                splashColor: Colors.grey[300]?.withOpacity(0.7),
+                                splashFactory: InkRipple.splashFactory,
+                                onTap: () => _pressShutter(),
+                                child: const SizedBox(width: 56, height: 56),
+                              ),
+                            ),
+                          )
+                        : Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () => _takePicture(),
+                                  child: const Text('save'),
+                                ),
+                              ),
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () => _deletePicture(),
+                                  child: const Text('not save'),
+                                ),
+                              ),
+                            ],
+                          ),
                   ),
                 ),
               ),
