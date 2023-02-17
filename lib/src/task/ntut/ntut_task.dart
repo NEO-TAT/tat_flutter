91ba790a620c0fd10445c07a6b40085f79863a82
Xanonymous <trusaidlin@gmail.com>
2023-02-18 07:04:22 +0800

    refactor: refine handle login steps with new passwordExpired state

--- lib/src/task/ntut/ntut_task.dart
+++ lib/src/task/ntut/ntut_task.dart
@@ -8,7 +8,7 @@ import 'package:flutter_app/src/connector/ntut_connector.dart';
 import 'package:flutter_app/src/r.dart';
 import 'package:flutter_app/src/store/local_storage.dart';
 import 'package:flutter_app/src/task/task.dart';
-import 'package:flutter_app/ui/other/error_dialog.dart';
+import 'package:flutter_app/ui/other/msg_dialog.dart';
 import 'package:flutter_app/ui/other/route_utils.dart';
 import 'package:tat_core/core/portal/domain/simple_login_result.dart';
 
@@ -58,13 +58,16 @@ class NTUTTask<T> extends DialogTask<T> {
   /// If the login status is false, we will force the user to login again.
   /// Otherwise, we will show the error dialog to the user, but still let the user to continue the task.
   Future<TaskStatus> _handleConnectorStatus(AccountStatus status) async {
-    final parameter = ErrorDialogParameter(
+    final parameter = MsgDialogParameter(
       desc: R.current.unknownServerError,
       dialogType: DialogType.warning,
-      offCancelBtn: true,
+      removeCancelButton: true,
     );
 
     TaskStatus taskStatus = TaskStatus.shouldIgnore;
+    bool shouldShowDialog = false;
+    bool shouldLogout = false;
+    bool shouldWipeData = false;
 
     switch (status) {
       case AccountStatus.normal:
@@ -73,29 +76,49 @@ class NTUTTask<T> extends DialogTask<T> {
         break;
       case AccountStatus.locked:
         // If the status is locked, we will prepare to show the error dialog.
+        shouldShowDialog = true;
         // And we will not allow the user to continue the task.
         parameter.desc = R.current.accountLock;
         taskStatus = TaskStatus.shouldGiveUp;
+        // Force to mark the login status as false, so that the user will be forced to login again.
+        shouldLogout = true;
         break;
       case AccountStatus.receivedInvalidCredential:
         // If the status is credential invalid, we will prepare to show the error dialog.
+        shouldShowDialog = true;
         // And we will not allow the user to continue the task.
         parameter.desc = R.current.accountPasswordError;
-        parameter.btnOkText = R.current.restart;
+        parameter.okButtonText = R.current.restart;
         parameter.dialogType = DialogType.error;
         taskStatus = TaskStatus.shouldGiveUp;
+        // Force to mark the login status as false, so that the user will be forced to login again.
+        shouldLogout = true;
+        shouldWipeData = true;
         break;
-      case AccountStatus.needsResetPassword:
+      case AccountStatus.passwordExpired:
         // If the status is password expired, we will prepare to show the error dialog.
+        shouldShowDialog = true;
+        // And we will not allow the user to continue the task.
+        parameter.desc = R.current.passwordExpiredWarning;
+        parameter.title = R.current.warning;
+        taskStatus = TaskStatus.shouldGiveUp;
+        // Force to mark the login status as false, so that the user will be forced to login again.
+        shouldLogout = true;
+        // Note that we don't need to wipe the data here, since the user can login again with the new password.
+        // This is different from the credential invalid case.
+        break;
+      case AccountStatus.passwordWillExpired:
+        // If the status is password expired, we will prepare to show the error dialog.
+        shouldShowDialog = true;
         // But we will still let the user to continue the task, since the user may want to change the password,
         // or the password expiration time is not come yet.
-        // TODO: analyze the password `really` expired response from the server.
-        parameter.desc = R.current.passwordExpiredWarning;
+        parameter.desc = R.current.passwordWillExpiredWarning;
         parameter.title = R.current.warning;
         taskStatus = TaskStatus.success;
         break;
       case AccountStatus.needsVerifyMobile:
         // If the status is mobile not verified, we will prepare to show the error dialog.
+        shouldShowDialog = true;
         // But we will still let the user to continue the task, since the user may not want to verify the mobile.
         parameter.desc = R.current.needsVerifyMobileWarning;
         parameter.dialogType = DialogType.info;
@@ -104,22 +127,23 @@ class NTUTTask<T> extends DialogTask<T> {
         break;
       default:
         // If the status is unknown, we will prepare to show the error dialog.
+        shouldShowDialog = true;
         parameter.desc = R.current.unknownServerError;
         break;
     }
 
-    // We will logout the user when the server returned unsuccessful result.
-    if (!_isLogin) {
+    if (shouldWipeData) {
       LocalStorage.instance.logout();
-      RouteUtils.toLoginScreen();
+    }
 
-      if (status != AccountStatus.normal) {
-        return onErrorParameter(parameter);
-      }
-    } else if (status != AccountStatus.normal && status != AccountStatus.needsVerifyMobile) {
-      // We will prevent to show the error dialog when the status is normal,
-      // or mobile verify requested.
-      ErrorDialog(parameter).show();
+    if (shouldLogout) {
+      RouteUtils.toLoginScreen();
+      // This is aim to invalidate the login status, prevent it use the previous one.
+      _isLogin = false;
+    }
+
+    if (shouldShowDialog) {
+      return await msgDialogShownResult(msgDialogParam: parameter, result: taskStatus);
     }
 
     return taskStatus;
@@ -133,7 +157,7 @@ class NTUTTask<T> extends DialogTask<T> {
   }
 
   @override
-  Future<TaskStatus> onErrorParameter(ErrorDialogParameter parameter) {
+  Future<TaskStatus> onErrorParameter(MsgDialogParameter parameter) {
     // This is aim to invalidate the login status, make it trigger the login task again.
     _isLogin = false;
     return super.onErrorParameter(parameter);
