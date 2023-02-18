89951a3542554a5930593d5c800b48a1feb787ce
Xanonymous <trusaidlin@gmail.com>
2023-02-18 22:21:10 +0800

    refactor: migrate custom dialog to null-safety

--- lib/ui/other/my_progress_dialog.dart
+++ lib/ui/other/my_progress_dialog.dart
@@ -1,54 +1,41 @@
-// TODO: remove sdk version selector after migrating to null-safety.
-// @dart=2.10
-import 'dart:io';
-
 import 'package:bot_toast/bot_toast.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'custom_progress_dialog.dart';
+import 'package:flutter_app/ui/other/custom_progress_dialog.dart';
 
 class MyProgressDialog {
-  static void showProgressDialogOld(BuildContext context, String message) async {}
-
-  static void progressDialog(String message) async {
-    BotToast.showCustomLoading(toastBuilder: (cancel) {
-      return CustomProgressDialog(
+  static void progressDialog(String? message) {
+    BotToast.showCustomLoading(
+      toastBuilder: (cancel) => CustomProgressDialog(
         child: Container(
-          decoration: const BoxDecoration(color: Colors.black, borderRadius: BorderRadius.all(Radius.circular(5))),
+          decoration: const BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
           padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Platform.isIOS
-                  ? const CupertinoActivityIndicator(
-                      radius: 15,
-                    )
-                  : const CircularProgressIndicator(),
-              message == null
-                  ? const Padding(
-                      padding: EdgeInsets.all(0),
-                    )
-                  : Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Text(
-                        message,
-                        style: const TextStyle(color: Colors.white),
-                        textAlign: TextAlign.center,
+          child: FittedBox(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const CircularProgressIndicator(),
+                message == null
+                    ? const SizedBox.shrink()
+                    : Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Text(
+                          message,
+                          style: const TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                    ),
-            ],
+              ],
+            ),
           ),
         ),
-      );
-    });
+      ),
+    );
   }
 
-  static Future<void> hideProgressDialog() async {
-    BotToast.cleanAll();
-  }
+  static void hideProgressDialog() => BotToast.cleanAll();
 
-  static Future<void> hideAllDialog() async {
-    BotToast.cleanAll();
-  }
+  static void hideAllDialog() => BotToast.cleanAll();
 }
