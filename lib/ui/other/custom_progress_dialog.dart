68178b1fe41675606001436db52dd011b31ded4e
TU-Lin <trusaidlin@gmail.com>
2023-02-18 22:54:22 +0800

    Fix circular progress indicator UI (#172)
    * refactor: migrate custom dialog to null-safety
    
    * feat: change color of circular progress indicator in course table page appbar

--- lib/ui/other/custom_progress_dialog.dart
+++ lib/ui/other/custom_progress_dialog.dart
@@ -1,20 +1,18 @@
-// TODO: remove sdk version selector after migrating to null-safety.
-// @dart=2.10
 import 'package:flutter/material.dart';
 
 class CustomProgressDialog extends StatelessWidget {
   const CustomProgressDialog({
-    Key key,
+    super.key,
     this.child,
     this.insetAnimationDuration = const Duration(milliseconds: 100),
     this.insetAnimationCurve = Curves.decelerate,
     this.shape,
-  }) : super(key: key);
+  });
 
   /// The widget below this widget in the tree.
   ///
   /// {@macro flutter.widgets.child}
-  final Widget child;
+  final Widget? child;
 
   /// The duration of the animation to show when the system keyboard intrudes
   /// into the space that the dialog is placed in.
@@ -35,37 +33,33 @@ class CustomProgressDialog extends StatelessWidget {
   ///
   /// The default shape is a [RoundedRectangleBorder] with a radius of 2.0.
   /// {@endtemplate}
-  final ShapeBorder shape;
+  final ShapeBorder? shape;
 
-  Color _getColor(BuildContext context) {
-    return Theme.of(context).dialogBackgroundColor;
-  }
+  Color _getColor(BuildContext context) => Theme.of(context).dialogBackgroundColor;
 
   @override
-  Widget build(BuildContext context) {
-    return AnimatedPadding(
-      padding: MediaQuery.of(context).viewInsets + const EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0),
-      duration: insetAnimationDuration,
-      curve: insetAnimationCurve,
-      child: MediaQuery.removeViewInsets(
-        removeLeft: true,
-        removeTop: true,
-        removeRight: true,
-        removeBottom: true,
-        context: context,
-        child: Center(
-          child: ConstrainedBox(
-            constraints: const BoxConstraints(),
-            child: Material(
-              elevation: 24.0,
-              color: _getColor(context),
-              type: MaterialType.card,
-              borderRadius: const BorderRadius.all(Radius.circular(5)),
-              child: child,
+  Widget build(BuildContext context) => AnimatedPadding(
+        padding: MediaQuery.of(context).viewInsets + const EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0),
+        duration: insetAnimationDuration,
+        curve: insetAnimationCurve,
+        child: MediaQuery.removeViewInsets(
+          removeLeft: true,
+          removeTop: true,
+          removeRight: true,
+          removeBottom: true,
+          context: context,
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(),
+              child: Material(
+                elevation: 24.0,
+                color: _getColor(context),
+                type: MaterialType.card,
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+                child: child,
+              ),
             ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
