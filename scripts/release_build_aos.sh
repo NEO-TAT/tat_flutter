ce9584d367f4eee1b0c67a7daacfbc064fd9cfbb
Xanonymous <trusaidlin@gmail.com>
2023-03-29 04:26:05 +0800

    chore: update release build script

--- scripts/release_build_aos.sh
+++ scripts/release_build_aos.sh
@@ -5,4 +5,9 @@ readonly SCRIPTS_DIR="$(dirname "$0")"
 flutter pub get && \
 bash "$SCRIPTS_DIR"/format.sh && \
 flutter analyze && \
-flutter build appbundle --split-debug-info=build/app/outputs/symbols --obfuscate
+flutter build appbundle -v --release --flavor real \
+  --deferred-components \
+  --validate-deferred-components \
+  --null-assertions \
+  --no-track-widget-creation \
+  --split-debug-info=build/app/outputs/symbols --obfuscate
--- scripts/release_build_ios.sh
+++ scripts/release_build_ios.sh
@@ -5,4 +5,7 @@ readonly SCRIPTS_DIR="$(dirname "$0")"
 flutter pub get && \
 bash "$SCRIPTS_DIR"/format.sh && \
 flutter analyze && \
-flutter build ipa --split-debug-info=build/app/outputs/symbols --obfuscate
+flutter build ipa -v --release --flavor real \
+  --export-method app-store \
+  --null-assertions \
+  --split-debug-info=build/app/outputs/symbols --obfuscate
