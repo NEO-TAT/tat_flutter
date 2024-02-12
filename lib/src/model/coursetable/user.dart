33f07fea544703bc3d1d2bf6831c82f5d65a3b1d
ntut-xuan <t109590031@ntut.org.tw>
2024-02-13 01:01:27 +0800

    Chore: Add final

--- lib/src/model/coursetable/user.dart
+++ lib/src/model/coursetable/user.dart
@@ -4,20 +4,16 @@ part 'user.g.dart';
 
 @JsonSerializable()
 class User {
-  String id;
-  String name;
-  String className;
+  final String id;
+  final String name;
+  final String className;
 
   User.origin()
       : id = "",
         name = "",
         className = "";
 
-  User({required this.id, required this.name, required this.className}) {
-    id = id;
-    name = name;
-    className = className;
-  }
+  User({required this.id, required this.name, required this.className});
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
   Map<String, dynamic> toJson() => _$UserToJson(this);
