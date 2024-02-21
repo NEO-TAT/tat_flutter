6ba666079e9cce5c56b367b2c3b896557a615cd1
James-Lu-none <jamesabcde277@gmail.com>
2024-02-21 15:13:24 +0800

    add error handling

--- lib/src/task/iplus/iplus_system_task.dart
+++ lib/src/task/iplus/iplus_system_task.dart
@@ -23,9 +23,14 @@ class IPlusSystemTask<T> extends NTUTTask<T> {
       final studentId = LocalStorage.instance.getAccount();
       final value = await ISchoolPlusConnector.login(studentId);
       super.onEnd();
-
-      if (value != ISchoolPlusConnectorStatus.loginSuccess) {
-        return onError(R.current.loginISchoolPlusError);
+      
+      switch(value){
+        case ISchoolPlusConnectorStatus.loginGetSSOIndexError:
+          return onError("ischool login get SSO index error");
+        case ISchoolPlusConnectorStatus.loginRedirectionError:
+          return onError("ischool login redirection error"); 
+        default:
+          break;
       }
     }
     return status;
