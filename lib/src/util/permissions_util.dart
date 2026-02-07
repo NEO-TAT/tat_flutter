6b0e3457a56b4989b875c6f0b2a37df551d9a5da
Riley Ho <riley@rileychh.dev>
2026-02-07 22:53:27 +0800

    fix: remove unnecessary media permissions to comply with Google Play policy

--- lib/src/util/permissions_util.dart
+++ lib/src/util/permissions_util.dart
@@ -6,38 +6,20 @@ import 'package:permission_handler/permission_handler.dart';
 class PermissionsUtil {
   /// Checks if the APP can access to the file sys on the current device.
   static Future<bool> checkHasAosStoragePermission() async {
-    // On iOS, the permission is managed by xcode project config.
     if (Platform.isIOS) return true;
     assert(Platform.isAndroid, 'The platform most be either aos or ios.');
 
     final deviceInfoPlugin = DeviceInfoPlugin();
     final androidSdkVersion = (await deviceInfoPlugin.androidInfo).version.sdkInt;
-    final requiredPermissions = <Permission>[];
 
-    // About the new `Granular media permissions` policy:
-    // Above Android API 33, the permission of storage is split into image/videos and music/audio.
-    // Which means we can't request the `READ_EXTERNAL_STORAGE`.
-    // And since the `permission_handler v10.2.0` still not support the new policy,
-    // we have to request the separate permissions.
-    // Please refer to https://developer.android.com/about/versions/13/behavior-changes-13#granular-media-permissions
-    if (androidSdkVersion >= 33) {
-      requiredPermissions.addAll([
-        Permission.videos,
-        Permission.audio,
-      ]);
-    } else {
-      requiredPermissions.addAll([
-        Permission.storage,
-      ]);
+    // On Android 10+ (API 29+), app-scoped storage (getExternalStorageDirectory)
+    // does not require any runtime permissions.
+    if (androidSdkVersion >= 29) {
+      return true;
     }
 
-    await requiredPermissions.request();
-    final determineResult = await Future.wait(requiredPermissions.map((permission) => permission.isGranted));
-
-    // Since the requiredPermissions is a list, user can choose to grant or deny any of them.
-    // So we have to check if any of them (instead of `every`) is granted.
-    // This depends on all the permissions in the list are able to make the App
-    // access the device's storage once they've been granted.
-    return determineResult.any((permission) => permission);
+    // On older Android versions, WRITE_EXTERNAL_STORAGE is needed.
+    final status = await Permission.storage.request();
+    return status.isGranted;
   }
 }
